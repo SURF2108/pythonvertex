ac42288ab35d3008306ca269f8dcab5ae188a3f6
SURF2108 <surabhi210806@gmail.com>
2025-01-30 16:39:29 +0530

    30.01.25

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,6 @@
 a = '10'
 if a.isdigit and (int(a))>0 :
-	print(a," is positive")
+	print(a," is positive") #for when you wish to print an integer alongside a string statement
 elif (int(a))<0:
 	print("a is negative")
 else:
