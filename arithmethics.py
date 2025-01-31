1884b47ef16b703c65d95bc0f91f6cb5afd1bfdf
SURF2108 <surabhi210806@gmail.com>
2025-02-01 01:40:34 +0530

    1.2.25

--- arithmethics.py
+++ arithmethics.py
@@ -1,10 +1,10 @@
-#WAP to split the tip of a bill
+#WAP to split the total bill, including the tip
 # input: bill, number of people, tip percentage
 
 bill = int(input("Enter the Total amount on bill: "))
 n = int(input("Enter the number of people to split among: "))
 t = float(input("Enter percentage of tip: "))
 
-split = ((bill*(t/100))+bill)/n
+bill+= ((bill*(t/100)))
 
-print("Split on tip: ",split)
+print("Split on bill, including tip : ",bill/n)
